4775bb2d1e55c67d26ef324230548a7a2430cbe2
dulleydoo <107411441+dulleydoo@users.noreply.github.com>
2022-07-02 13:04:26 -0400

    completed Assignment

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -12,6 +12,11 @@ public class PersonHandler {
 
     public String whileLoop() {
         String result = "";
+        int counter = 0;
+        while (counter < personArray.length) {
+            result += (personArray[counter].toString());
+            counter++;
+        }
         // create a `counter`
         // while `counter` is less than length of array
             // begin loop
@@ -28,17 +33,20 @@ public class PersonHandler {
 
     public String forLoop() {
         String result = "";
-        // identify initial value
-        // identify terminal condition
-        // identify increment
+        for (int start = 0; start <= personArray.length-1 ; start++) {
 
-        // use the above clauses to declare for-loop signature
+            result += (personArray[start].toString());
+            // identify initial value
+            // identify terminal condition
+            // identify increment
+
+            // use the above clauses to declare for-loop signature
             // begin loop
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
+            // use `counter` to identify the `current Person` in the array
+            // get `string Representation` of `currentPerson`
+            // append `stringRepresentation` to `result` variable
             // end loop
-
+        }
         return result;
     }
 
@@ -46,6 +54,9 @@ public class PersonHandler {
 
     public String forEachLoop() {
         String result = "";
+        for (Person person : personArray) {
+            result += person;
+        }
         // identify array's type
         // identify array's variable-name
 
